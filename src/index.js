44d7ae290f202428a356bdd0705fe914363f9172
Jovan-Nsty <jova.zyo@gmail.com>
2024-02-13 14:00:44 +0100

    Added home page design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('called from index.js');
+import { paraArray } from './para.js';
+
+const content = document.getElementById('content');
+
+function updateDOM() {
+    const homeDiv = document.createElement('div');
+    homeDiv.setAttribute('id', 'home');
+
+    const header = document.createElement('h1');
+    header.textContent = 'Golden Hive Delights';
+
+    homeDiv.appendChild(header);
+
+    for(let i = 0; i < paraArray.length; i++) {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = paraArray[i];
+        homeDiv.appendChild(paragraph);
+    }
+    
+    return homeDiv;
+}
+
+content.appendChild(updateDOM());
